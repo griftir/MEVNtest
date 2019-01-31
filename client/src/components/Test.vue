d6bc573b14ee69cc530813ecd9ce694eaec5cc41
Magno <1387550050@mil>
2019-01-31 14:15:34 -0500

    basic vue client and server

---
+++ client/src/components/Test.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="test">
+    <h1>Test</h1>
+    TEST
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Test',
+  data () {
+    return {
+      msg: 'TEST TEST TEST'
+    }
+  }
+}
+</script>
